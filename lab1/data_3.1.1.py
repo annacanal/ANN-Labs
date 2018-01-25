9b3832bce2991849092dd6a9377af545fbc436ea
jenny <stoby.jenny@gmail.com>
2018-01-25 23:16:38 +0100

    Generate data, no shuffling, in 3.1.1.

---
+++ lab1/data_3.1.1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+mean_x = [-20, -20]
+mean_y = [20, 20]
+cov_x = [[100, 0], [0, 100]]
+cov_y = [[100, 0], [0, 100]]
+N = 100
+
+x1, x2 = np.random.multivariate_normal(mean_x, cov_x, N).T
+y1, y2 = np.random.multivariate_normal(mean_y, cov_y, N).T
+
+
+plt.plot(x1, x2,  'x')
+plt.plot(y1, y2, 'o')
+plt.axis('equal')
+plt.show()
