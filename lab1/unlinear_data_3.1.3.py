d72840f5433ba0705be900ad9f7ec24b80975318
jenny <stoby.jenny@gmail.com>
2018-01-26 10:27:12 +0100

    The unlinearly seperable data

---
+++ lab1/unlinear_data_3.1.3.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+mean_x = [15, 0]
+mean_y = [0, 15]
+cov_x = [[100, 0], [0, 100]]
+cov_y = [[100, 0], [0, 100]]
+N = 100
+
+
+x1, x2 = np.random.multivariate_normal(mean_x, cov_x, N).T
+y1, y2 = np.random.multivariate_normal(mean_y, cov_y, N).T
+
+classA = np.column_stack((x1,x2)).T
+classB = np.column_stack((y1,y2)).T 
+
+X = np.append(classA[0], classB[0])
+Y = np.append(classA[1], classB[1])
+T =  np.append(np.ones(len(classA[0])), -np.ones(len(classB[0])))
+bias = np.ones(2*N)
+
+s = np.random.permutation(2*N)
+patterns = np.concatenate(([X[s]], [Y[s]], [bias]), axis=0)
+target =T[s]
+
+
+plt.plot(x1, x2,  'x')
+plt.plot(y1, y2, 'o')
+plt.axis('equal')
+plt.show()
