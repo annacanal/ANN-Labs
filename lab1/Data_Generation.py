05c8aa5141fd29b9f7eaee95ebb04ee71819dd4e
annacanal <31701548+annacanal@users.noreply.github.com>
2018-01-29 14:49:04 +0100

    Update Data_Generation.py

--- lab1/Data_Generation.py
+++ lab1/Data_Generation.py
@@ -1,17 +1,21 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def generate_linearData():
-    mean_x = [-30, -30]
-    mean_y = [30, 30]
-    cov_x = [[100, 0], [0, 100]]
-    cov_y = [[100, 0], [0, 100]]
-    N = 5
+    mean_x = [-3, -3]
+    mean_y = [3, 3]
+    cov_x = [[1, 0], [0, 1]]
+    cov_y = [[1, 0], [0, 1]]
+    N = 100
 
-    x1, x2 = np.random.multivariate_normal(mean_x, cov_x, N).T
-    y1, y2 = np.random.multivariate_normal(mean_y, cov_y, N).T
+    x1, y1 = np.random.multivariate_normal(mean_x, cov_x, N).T
+    x2, y2 = np.random.multivariate_normal(mean_y, cov_y, N).T
 
-    classA = np.column_stack((x1, x2)).T
-    classB = np.column_stack((y1, y2)).T
+    classA = np.column_stack((x1, y1)).T
+    classB = np.column_stack((x2, y2)).T
+
+    # classA = np.row_stack((x1, y1))
+    # classB = np.row_stack((x2, y2))
 
     X = np.append(classA[0], classB[0])
     Y = np.append(classA[1], classB[1])
@@ -22,20 +26,20 @@ def generate_linearData():
     patterns = np.concatenate(([X[s]], [Y[s]], [bias]), axis=0)
     target = T[s]
 
-    print(patterns)
-    print(target)
+   # print(patterns)
+    #print(target)
 
-    # plt.plot(x1, x2,  'x')
-    # plt.plot(y1, y2, 'o')
+    # plt.plot(x1, y1,  'x')
+    # plt.plot(x2, y2, 'o')
     # plt.axis('equal')
     # plt.show()
     return patterns, target
 
 def generate_nonlinearData():
-    mean_x = [15, 0]
-    mean_y = [0, 15]
-    cov_x = [[100, 0], [0, 100]]
-    cov_y = [[100, 0], [0, 100]]
+    mean_x = [3, 3]
+    mean_y = [1, 1]
+    cov_x = [[1, 0], [0, 1]]
+    cov_y = [[1, 0], [0, 1]]
     N = 100
 
     x1, x2 = np.random.multivariate_normal(mean_x, cov_x, N).T
@@ -52,4 +56,9 @@ def generate_nonlinearData():
     s = np.random.permutation(2 * N)
     patterns = np.concatenate(([X[s]], [Y[s]], [bias]), axis=0)
     target = T[s]
+
+    # plt.plot(x1, y1,  'x')
+    # plt.plot(x2, y2, 'o')
+    # plt.axis('equal')
+    # plt.show()
     return patterns, target
