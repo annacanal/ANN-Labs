7d0a9003f392c1105997b718e31dbbe10e31cb75
jenny <stoby.jenny@gmail.com>
2018-01-29 19:21:33 +0100

    Update to functioon approx

--- lab1/Function_Approximation.py
+++ lab1/Function_Approximation.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from Two_Layer_Perceptron import forward_pass
+from Two_Layer_Perceptron import forward_pass, W_init
 
 def approx_function():
     x = np.arange(-5, 5, 0.5)
@@ -9,6 +9,7 @@ def approx_function():
     N = len(x)*len(y)
     z = np.exp((np.outer(x,x)+np.outer(y,y))/10) - 0.5
     xx, yy = np.meshgrid(x, y)
+    print(len(xx))
     #targets and patterns for training:
     targets = z.reshape(1, N)
     patterns = np.concatenate((xx.reshape(1, N), yy.reshape(1, N)), axis=0)
@@ -18,11 +19,17 @@ def approx_function():
     # surf = ax.plot_surface(xx, yy, z)
     # plt.show()
 
-    #Through forward pass get out with pattern as input data: Experiment with different number of nodes
-    n_nodes = 3
-    V,W,H,O = forward_pass(patterns, n_nodes)
-    print(O)
-    # zz = out.reshape
+    #Through forward pass get out with "approx function"s patterns as input:
+    n_nodes = 2 #doesn't do anything yet - but we do want to try for different number of nodes
+    W = W_init(n_nodes, patterns)
+    V = W_init(np.ones((2,200)))
+    W,V,H,O = forward_pass(patterns, W, V)
+    zz = O.reshape(len(xx),len(yy))
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(xx, yy, zz)
+    plt.show()
+
 
 def main():
     approx_function()
