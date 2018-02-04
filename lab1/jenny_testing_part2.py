ce8c0be34f41fd332dac4e94e6e88942ee6aff6c
jenny <stoby.jenny@gmail.com>
2018-02-04 15:38:25 +0100

    Update

--- lab1/jenny_testing_part2.py
+++ lab1/jenny_testing_part2.py
@@ -4,20 +4,15 @@ import matplotlib.pyplot as plt
 import Mackey_Glass_Datapoints
 import Evaluation
 
-def regression_evaluation(n_nodes, xtrain, ytrain, xtest, ytesttrue, xval, yval, alpha_list):
-    error_mse_test = np.zeros(len(alpha_list))
-    for i in range(len(alpha_list)):
-        nn = MLPRegressor(
-        hidden_layer_sizes=(n_nodes,),  activation='logistic', solver='sgd', alpha=alpha_list[i], #batch_size='auto',
-        learning_rate='constant', learning_rate_init=0.001, power_t=0.5, max_iter=100000, #shuffle=True,
-        random_state=9, tol=0.0001, verbose=False, warm_start=False, momentum=0.9, nesterovs_momentum=True,
-        early_stopping=True, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-        n = nn.fit(xtrain, ytrain)
-        # ytrain = nn.predict(xtrain) 
-        yval_predicted = n.predict(xval)
-        # error_mse = Evaluation.mean_sq_error(ytrain, ytesttrue)
-        error_mse_test[i] = Evaluation.mean_sq_error(yval_predicted, yval)
-    return error_mse_test
+def regression_evaluation(n_nodes, xtrain, ytrain, x, alpha_list):
+    nn = MLPRegressor(
+    hidden_layer_sizes=(n_nodes,),  activation='logistic', solver='sgd', alpha=alpha_list, #batch_size='auto',
+    learning_rate='constant', learning_rate_init=0.001, power_t=0.5, max_iter=100000, #shuffle=True,
+    random_state=9, tol=0.0001, verbose=False, warm_start=False, momentum=0.9, nesterovs_momentum=True,
+    early_stopping=True, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    n = nn.fit(xtrain, ytrain)
+    y = n.predict(x)
+    return y
 
 def main():
     i = np.arange(301, 1500, 5)
@@ -41,24 +36,40 @@ def main():
     # print(ytesttrue)
 
     n_nodes = np.array([2, 3, 5, 6, 7])
-    best_node = 2
-    alpha_list = np.linspace(0.001, 0.1, 10)
+    alpha_list = np.linspace(0.001, 0.1, 8)
     errors_val = []
 
-    for i in range(len(n_nodes)):
-        error_val = regression_evaluation(n_nodes[i], xtrain, ytrain, xtest, ytesttrue, xval, yval, alpha_list)
-        errors_val.append(error_val)
+# Task 4.3.1_3 (Question 3) - training and then decide best model from validation set: 
+    # for i in range(len(n_nodes)):
+    #     error_val = np.zeros(len(alpha_list))
+    #     for j in range(len(alpha_list)):
+    #         y_predicted = regression_evaluation(n_nodes[i], xtrain, ytrain, xval, alpha_list[j])
+    #         error_val[j] = Evaluation.mean_sq_error(y_predicted, yval)
+    #     errors_val.append(error_val)
 
-    error_test = regression_evaluation(best_node, xtrain, ytrain, xtest, ytesttrue, xval, yval, alpha_list)
+    # for i in range(len(n_nodes)):
+    #     name= "Validation set"
+    #     plt.title(name)
+    #     plt.plot(alpha_list, errors_val[i], label= "Nodes = "+str(n_nodes[i]))
+    #     plt.xlabel('Alphas')
+    #     plt.ylabel('Error_mse')
+    #     plt.legend()
+    # plt.show()
 
-    for i in range(len(n_nodes)):
-        name= "MSE/iteration in test"
-        plt.title(name)
-        plt.plot(alpha_list, errors_test[i], label= "Nodes = "+str(n_nodes[i]))
-        plt.xlabel('Alphas')
-        plt.ylabel('Error_mse')
-        plt.legend()
-    plt.show()
+# Task 4.3.1_4 (Question 4) - Choosing best model and plot: 
+    best_node = 2
+    best_alpha = 0.001
+    y_predicted = regression_evaluation(best_node, xtrain, ytrain, xtest, best_alpha)
+    error_val = Evaluation.mean_sq_error(y_predicted, yval)
+    print(error_val)
+
+    # name= "Test set"
+    # plt.title(name)
+    # plt.plot(xtest, errors_val[i])
+    # plt.xlabel('xtest')
+    # plt.ylabel('Error_mse')
+    # plt.legend()
+    # plt.show()
 
 if __name__ == "__main__":
     main()
