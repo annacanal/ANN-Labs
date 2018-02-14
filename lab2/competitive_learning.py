e0cc170c931af60d42d442af5e1df3998dbe9621
jenny <stoby.jenny@gmail.com>
2018-02-14 15:29:01 +0100

    Update competitive learning

---
+++ lab2/competitive_learning.py
@@ -0,0 +1,139 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPRegressor
+
+def datashuffler(train, test, target_1, test_target_1):
+
+    # Shuffle data
+    index_train = np.random.permutation(len(train))
+    index_test = np.random.permutation(len(test))
+    #np.random.shuffle(train)
+    train = train[index_train]
+    test=test[index_test]
+    target_1 = target_1[index_train]
+    test_target_1=test_target_1[index_test]
+
+    return (train, test, target_1, test_target_1)
+
+
+def data(noise):
+    train = np.arange(0, 2*np.pi, 0.1)
+    test = np.arange(0.05, 2*np.pi, 0.1)
+
+    #Adding noise with variance=0.1
+    if (noise==1):
+        noise_train = np.random.normal(0, 0.1, len(train))
+        noise_test = np.random.normal(0, 0.1, len(test))
+        train = train + noise_train
+        test = test + noise_test
+
+    target_1 = sin_function(train)
+    test_target_1 = sin_function(test)
+
+    return train, test, target_1, test_target_1
+
+def weights_init(x):
+    weights = np.random.rand(len(x))
+    return weights
+
+def sin_function(x):
+    y = np.zeros(len(x))
+    for i in range(len(x)):
+        y[i] = np.sin(2*x[i])
+    return y
+
+def phi_function(x, mu, sigma):
+    phi = np.exp(-(x-mu)**2/(2*sigma**2))
+    return phi
+
+def phi_vector(xi, mu, sigma):
+    phi_vector = np.zeros(len(mu))
+    for i in range(len(mu)):
+        phi_vector[i] = phi_function(xi, mu[i], sigma)
+    return phi_vector
+
+def error_function(target, phi_vector, weights):
+    error= (target - np.matmul(phi_vector,np.transpose(weights)))
+    # print(error)
+    #error = (target - np.dot(weights,phi_vector.T))
+    return error
+
+def chunkify(seq, num):
+    out = []
+
+    if len(seq) % num == 0:
+        length = len(seq) / num
+        plus = 0
+    else:
+        length = len(seq) / num
+        plus = len(seq) % num
+
+    howmany = 0
+    while howmany < len(seq):
+        first = howmany
+        last = first + length
+        if plus > 0:
+            last += 1
+            plus -= 1
+        out.append(seq[int(first):int(last)])
+        howmany += (last - first)
+    return out
+
+def init_mus(nodes_number, train):
+    mus = np.zeros(nodes_number)
+
+    chunks = chunkify(train, nodes_number)
+    for i,elem in enumerate(chunks):
+        mean = np.mean(elem)
+        mus[i] = mean
+    return mus
+
+def main():
+    eta = 0.0001
+    sigma_value=0.2
+    nodes= 20
+ 
+    epochs = 1
+    init_learning_rate = 0.2
+    init_radius = 50
+    time_constant = epochs / np.log(init_radius)
+
+    noise = 0 #noise=0 without noise, noise=1 for a gaussian noise
+    train, test, target_1, test_target_1 = data(noise)
+    mu = init_mus(nodes, train)
+
+    net = np.random.random((20, 63))
+
+    phi_vecs =[]
+    for j in range(len(train)):
+        phi = phi_vector(train[j], mu, sigma_value)
+        phi_vecs.append(phi)
+   
+    np.random.shuffle(phi_vecs)
+   
+    for i in range(epochs):
+        for j in range(len(phi_vecs)):
+            row_p = phi_vecs[j]
+
+    
+
+
+    # prediction = np.dot(phi_vecs,weights)
+    # name = type +" approximation, delta rule"
+    # plt.title(name)
+    # plt.scatter(train, prediction,s=2.5, label="Prediction")
+    # plt.scatter(train, target, s=2.5, label="Target")
+    # plt.legend()
+    # plt.show()
+    #
+    # iterations = np.arange(epochs)
+    # name= "Error/iteration delta rule"
+    # plt.title(name)
+    # plt.plot(iterations, errors,'blue')
+    # plt.xlabel('Epochs')
+    # plt.ylabel('Error')
+    # plt.show()
+
+
+if __name__ == "__main__":
+    main()
--- lab2/votes_of_MPs.py
+++ lab2/votes_of_MPs.py
@@ -30,12 +30,12 @@ def get_data_matrix():
 
     return party_matrix, gender_matrix, district_matrix, vote_matrix
 
-def get_animal_names():
-    with open("data_lab2/animalnames.txt", "r") as f:
-        # Read the whole file at once
-        names = f.readlines()
-    names = [x.strip() for x in names]
-    return names
+# def get_animal_names():
+#     with open("data_lab2/animalnames.txt", "r") as f:
+#         # Read the whole file at once
+#         names = f.readlines()
+#     names = [x.strip() for x in names]
+#     return names
 
 def find_bmu(t, net):
     """
@@ -72,77 +72,78 @@ def calculate_influence(distance, radius):
     return np.exp(-distance / (2* (radius**2)))
 
 
-# def main():
-#     votes_data = get_data_matrix()
-#     epochs = 20
-#     init_learning_rate = 0.2
-#     # weight matrix (i.e. the SOM) needs to be one m-dimensional vector for each neuron in the SOM
-#     net = np.random.random((10,10,31))
-#     # initial neighbourhood radius
-#     init_radius = 5
-#     # radius decay parameter
-#     time_constant = epochs / np.log(init_radius)
-#     # Learning
-#     for i in range(epochs):
-#         # for each MP
-#         for j in range((votes_data.shape[0])):
-#             row_p = votes_data[j][:]
-#             bmu, bmu_idx = find_bmu(row_p, net)
+def main():
+    party_matrix, gender_matrix, district_matrix, votes_data = get_data_matrix()
+    epochs = 20
+    init_learning_rate = 0.2
+    # weight matrix (i.e. the SOM) needs to be one m-dimensional vector for each neuron in the SOM
+    net = np.random.random((10,10,31))
+    # initial neighbourhood radius
+    init_radius = 5
+    # radius decay parameter
+    time_constant = epochs / np.log(init_radius)
 
-#             r = decay_radius(init_radius, i, time_constant)
-#             l = decay_learning_rate(init_learning_rate, i, epochs)
-#             # now we know the BMU, update its
-#             # weight vector to move closer to input
-#             # and move its neighbours in 2-D space closer
-#             # by a factor proportional to their 2-D distance from the BMU
-#             for x in range(net.shape[0]):
-#                 for y in range(net.shape[1]):
-#                     w = net[x][y]#.reshape(1, 84)
-#                     # get the 2-D distance (again, not the actual Euclidean distance)
-#                     w_dist = np.sum(np.square(np.array([x,y]) - np.array(bmu_idx)))
-#                     # if the distance is within the current neighbourhood radius
-#                     if w_dist <= r ** 2:
-#                         # calculate the degree of influence (based on the 2-D distance)
-#                         influence = calculate_influence(w_dist, r)
-#                         # now update the neuron's weight using the formula:
-#                         # new w = old w + (learning rate * influence * delta)
-#                         # where delta = input vector (t) - old w
-#                         new_w = w + (l * influence * (row_p - w))
+    # Learning
+    for i in range(epochs):
+        # for each MP
+        for j in range((votes_data.shape[b0])):      #349
+            row_p = votes_data[j][:]
+            bmu, bmu_idx = find_bmu(row_p, net)
 
-#                         # commit the new weight
-#                         net[x][y] = new_w
+            r = decay_radius(init_radius, i, time_constant)
+            l = decay_learning_rate(init_learning_rate, i, epochs)
+            # now we know the BMU, update its
+            # weight vector to move closer to input
+            # and move its neighbours in 2-D space closer
+            # by a factor proportional to their 2-D distance from the BMU
+            for x in range(net.shape[0]):
+                for y in range(net.shape[1]):
+                    w = net[x][y]#.reshape(1, 84)
+                    # get the 2-D distance (again, not the actual Euclidean distance)
+                    w_dist = np.sum(np.square(np.array([x,y]) - np.array(bmu_idx)))
+                    # if the distance is within the current neighbourhood radius
+                    if w_dist <= r ** 2:
+                        # calculate the degree of influence (based on the 2-D distance)
+                        influence = calculate_influence(w_dist, r)
+                        # now update the neuron's weight using the formula:
+                        # new w = old w + (learning rate * influence * delta)
+                        # where delta = input vector (t) - old w
+                        new_w = w + (l * influence * (row_p - w))
 
-#     #display the results
-#     posx = []
-#     posy = []
-#     for i in range(votes_data.shape[0]):
-#         row_p = votes_data[i][:]
-#         bmu, bmu_idx = find_bmu(row_p, net)
-#         posx.append(bmu_idx[0])
-#         posy.append(bmu_idx[1])
+                        # commit the new weight
+                        net[x][y] = new_w
 
-#     plt.scatter(posx, posy)
-#     plt.show()
+    #display the results
+    posx = []
+    posy = []
+    for i in range(votes_data.shape[0]):
+        row_p = votes_data[i][:]
+        bmu, bmu_idx = find_bmu(row_p, net)
+        posx.append(bmu_idx[0])
+        posy.append(bmu_idx[1])
 
-#         #
-#     # # Find index for species
-#     # names = get_animal_names()
-#     # pos = []
-#     # idx_names = []
-#     # for i in range(len(animals_data)):
-#     #     row_p = animals_data[i][:]
-#     #     bmu, bmu_idx = find_bmu(row_p, net)
-#     #     idx_names.append(i)
-#     #     pos.append(bmu_idx)
-#     #
-#     # ordered_pos = sorted(zip(pos, idx_names))
-#     # print(ordered_pos)
-#     # names_order = []
-#     # for i in range(len(names)):
-#     #     pos = ordered_pos[i][1]
-#     #     names_order.append(names[pos])
-#     #     print(names_order[i])
-#     # # print(names_order)
+    plt.scatter(posx, posy)
+    plt.show()
 
-# if __name__ == "__main__":
-#      main()
+        #
+    # # Find index for species
+    # names = get_animal_names()
+    # pos = []
+    # idx_names = []
+    # for i in range(len(animals_data)):
+    #     row_p = animals_data[i][:]
+    #     bmu, bmu_idx = find_bmu(row_p, net)
+    #     idx_names.append(i)
+    #     pos.append(bmu_idx)
+    #
+    # ordered_pos = sorted(zip(pos, idx_names))
+    # print(ordered_pos)
+    # names_order = []
+    # for i in range(len(names)):
+    #     pos = ordered_pos[i][1]
+    #     names_order.append(names[pos])
+    #     print(names_order[i])
+    # # print(names_order)
+
+if __name__ == "__main__":
+     main()
