70e910f8344cf1a605e310b16f30638dc86825da
annacanal <31701548+annacanal@users.noreply.github.com>
2018-02-22 18:07:41 +0100

    one iteration

--- lab3/lab3_2.py
+++ lab3/lab3_2.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import lab3_1
 
 
 def read_pictData():
@@ -16,8 +16,6 @@ def read_pictData():
             patterns_matrix[i][j] = patterns_line[position]
     return patterns_matrix
 
-
-
 def patterns_transform(patterns_matrix):
     new_patterns = []
     for i in range(patterns_matrix.shape[0]):
@@ -26,10 +24,48 @@ def patterns_transform(patterns_matrix):
     return new_patterns
 
 
+def weight_matrix(nodes, patterns):
+    # # way 1
+    # W1 = np.zeros((Nodes, Nodes))
+    # for i in range(W1.shape[0]):
+    #     for j in range(W1.shape[1]):
+    #         for k in range(patterns.shape[0]):
+    #             W1[i][j] += (1 / Nodes) * patterns[k][i] * patterns[k][j]
+
+    W = np.zeros((nodes, nodes))
+    for k in range(len(patterns)):
+        W += (1 / nodes) * (np.outer(np.transpose(patterns[k]), patterns[k]))
+    return W
+
+
+def calc_activations(W, input_pattern):
+    output = np.sum(W * input_pattern, axis=1)
+    return output
+
+
 def main():
     patterns_matrix=read_pictData()
-    patterns_transform(patterns_matrix)
+    patterns = patterns_transform(patterns_matrix)
+    nodes=1024
 
+    #train with p1, p2, p3
+    train_patterns = [patterns[0], patterns[1], patterns[2]]
+    W = weight_matrix(nodes, train_patterns)
+
+    ### p11 and p22
+    print("p11")
+    print()
+    print( patterns_matrix[9])
+    print("p22")
+    print(patterns_matrix[10])
+    print()
+
+
+############################# outputs
+    output = calc_activations(W, patterns_matrix[9]) #check the p11 (which is the 10)
+    output2 = calc_activations(W, patterns_matrix[10]) #check the p22 (which is the 11)
+    print(output)
+    print(output2)
 
 if __name__ == "__main__":
     main()
