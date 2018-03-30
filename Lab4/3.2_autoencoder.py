7568fd558e7988c0a6f88f73a02ea917996e5f0c
jenny <stoby.jenny@gmail.com>
2018-03-30 15:17:13 +0200

    Fitting classifier and pre-training differently, with different epochs, learning rate.

--- Lab4/3.2_autoencoder.py
+++ Lab4/3.2_autoencoder.py
@@ -21,10 +21,7 @@ def evaluate(test_labels, predictions):
     print('mean recall: {}'.format(recall1))
     print('mean fscore: {}'.format(f1score1))
 
-
-
 #-----------pre-training------------------------
-
 input_img = Input(shape=(784,))
 
 #3 hidden layers: 
@@ -78,11 +75,11 @@ decoded = Dense(10, activation='sigmoid', kernel_initializer='random_normal', bi
 classifier = Model(input = input_img, output = decoded)
 
 #training classifier
-sgd = optimizers.SGD(lr=0.3, momentum=0, decay=0, nesterov=False)
+sgd = optimizers.SGD(lr=0.1, momentum=0, decay=0, nesterov=False)
 classifier.compile(optimizer=sgd, loss='mean_squared_error', metrics=['mae'])
 
 hist = classifier.fit(train, train_targets, 
-                epochs = 75, 
+                epochs = 20, 
                 batch_size = 1, 
                 shuffle = True,
                 validation_data=(test, test_targets))
@@ -95,70 +92,33 @@ decoded_imgs = decoder_2.predict(decoded_imgs)
 decoded_imgs = decoder_3.predict(decoded_imgs)
 
 evaluate(test, decoded_imgs.round()) #classifier or decdoded_imgs?
-
 #--------------------------------------------------------------------------------
+#Plotting digits: 
+n = 10  
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
-# n = 10  
-# plt.figure(figsize=(20, 4))
-# for i in range(n):
-#     ax = plt.subplot(2, n, i + 1)
-#     plt.imshow(test[i].reshape(28, 28))
-#     plt.gray()
-#     ax.get_xaxis().set_visible(False)
-#     ax.get_yaxis().set_visible(False)
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
-#     ax = plt.subplot(2, n, i + 1 + n)
-#     # plt.imshow(decoded_imgs[i].reshape(28, 28))
-#     plt.imshow(decoded_imgs[i].reshape(28, 28))
-#     plt.gray()
-#     ax.get_xaxis().set_visible(False)
-#     ax.get_yaxis().set_visible(False)
-
-# # np.save('3_hidden_layers_150_120_90_epoch50_lr01_batchsize1.npy', hist.history)
-# np.save('test_3_layers_75epochs_0.3lr.npy', hist.history)
-
-# # plt.figure(2)
-# # plt.plot(hist.history['mean_absolute_error'])
-
-# plt.show()
-
-
-# Plotting
-weights_3 = decoder_3.get_weights()[0]
-weights_2 = decoder_2.get_weights()[0]
-weights_1 = decoder_1.get_weights()[0]
-
-print(np.array(weights_3).shape)
-print(np.array(weights_2).shape)
-print(np.array(weights_1).shape)
-
-# print(np.array(weights[1]).shape)
-
-plt.figure(figsize=(10, 10))
-for i in range(150):
-    plt.subplot(10, 15, i + 1)
-    plt.imshow(weights_3[i].reshape((28, 28)), cmap=plt.cm.gray_r,
-                interpolation='nearest')
-    plt.xticks(())
-    plt.yticks(())
-plt.suptitle('150 components extracted by autoencoder layer 1', fontsize=16)
-plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
+    # np.save('test_3_layers_75epochs_0.3lr.npy', hist.history)
 plt.show()
 
-plt.figure(figsize=(10, 10))
-for i in range(120):
-    plt.subplot(10, 15, i + 1)
-    plt.imshow(weights_2[i].reshape((10, 15)), cmap=plt.cm.gray_r,
-                interpolation='nearest')
-    plt.xticks(())
-    plt.yticks(())
-plt.suptitle('120 components extracted by autoencoder layer 2', fontsize=16)
-plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
-plt.show()
+# # Plotting images for 3rd layer: 
+weights = encoder.get_weights()[0]
+print(np.array(weights).shape)
 
 plt.figure(figsize=(10, 10))
 for i in range(90):
-    plt.subplot(10, 15, i + 1)
+    plt.subplot(10, 10, i + 1)
     plt.imshow(weights_1[i].reshape((10, 12)), cmap=plt.cm.gray_r,
                 interpolation='nearest')
     plt.xticks(())
@@ -166,3 +126,33 @@ for i in range(90):
 plt.suptitle('90 components extracted by autoencoder layer 3', fontsize=16)
 plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
 plt.show()
+
+# weights_3 = decoder_3.get_weights()[0]
+# weights_2 = decoder_2.get_weights()[0]
+# weights_1 = decoder_1.get_weights()[0]
+
+# print(np.array(weights_3).shape)
+# print(np.array(weights_2).shape)
+# print(np.array(weights_1).shape)
+
+# plt.figure(figsize=(10, 10))
+# for i in range(150):
+#     plt.subplot(10, 15, i + 1)
+#     plt.imshow(weights_3[i].reshape((28, 28)), cmap=plt.cm.gray_r,
+#                 interpolation='nearest')
+#     plt.xticks(())
+#     plt.yticks(())
+# plt.suptitle('150 components extracted by autoencoder layer 1', fontsize=16)
+# plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
+# plt.show()
+
+# plt.figure(figsize=(10, 10))
+# for i in range(120):
+#     plt.subplot(10, 12, i + 1)
+#     plt.imshow(weights_2[i].reshape((10, 15)), cmap=plt.cm.gray_r,
+#                 interpolation='nearest')
+#     plt.xticks(())
+#     plt.yticks(())
+# plt.suptitle('120 components extracted by autoencoder layer 2', fontsize=16)
+# plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
+# plt.show()
