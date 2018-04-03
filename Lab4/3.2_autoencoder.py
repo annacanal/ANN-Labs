3863435d79b6bea8488c3dee70e7fc68424db398
jenny <stoby.jenny@gmail.com>
2018-04-04 01:01:59 +0200

    Update of deep autoencoder, added pictures

--- Lab4/3.2_autoencoder.py
+++ Lab4/3.2_autoencoder.py
@@ -7,153 +7,181 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from keras.utils import np_utils
 from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import classification_report
 
-def evaluate(test_labels, predictions):
-    precision, recall, f1score, support = precision_recall_fscore_support(test_labels, predictions)
-    precision1=np.mean(precision)
-    recall1=np.mean(recall)
-    f1score1=np.mean(f1score)
-    print("eval")
-    # print('precision: {}'.format(precision))
-    # print('recall: {}'.format(recall))
-    # print('fscore: {}'.format(f1score))
-    print('mean precision: {}'.format(precision1))
-    print('mean recall: {}'.format(recall1))
-    print('mean fscore: {}'.format(f1score1))
+def main():
+    train, train_targets = data_handling.read_train_dataset()
+    test, test_targets = data_handling.read_test_dataset()
+    input_img = Input(shape=(784,))
+    layers_list = 3
+    my_epochs = 500
+    # for layers in layers_list:
+    training(my_epochs, layers_list, input_img, train, test, train_targets, test_targets)
 
+
+def training(my_epochs, layers, input_img, train, test, train_targets, test_targets):
 #-----------pre-training------------------------
-input_img = Input(shape=(784,))
+    if layers == 3: 
+        encoded = Dense(150, activation = 'relu')(input_img)
+        encoded = Dense(120, activation = 'relu')(encoded)
+        encoded = Dense(90, activation = 'relu')(encoded)
 
-#3 hidden layers: 
-encoded = Dense(150, activation = 'relu', kernel_initializer='random_normal', bias_initializer='zeros')(input_img)
-encoded = Dense(120, activation = 'relu', kernel_initializer='random_normal', bias_initializer='zeros')(encoded)
-encoded = Dense(90, activation = 'relu', kernel_initializer='random_normal', bias_initializer='zeros')(encoded)
+        decoded = Dense(120, activation = 'relu')(encoded)
+        decoded = Dense(150, activation = 'relu')(decoded)
+        decoded = Dense(784, activation='sigmoid')(decoded)
 
-decoded = Dense(120, activation = 'relu', kernel_initializer='random_normal', bias_initializer='zeros')(encoded)
-decoded = Dense(150, activation = 'relu', kernel_initializer='random_normal', bias_initializer='zeros')(decoded)
-decoded = Dense(784, activation='sigmoid', kernel_initializer='random_normal', bias_initializer='zeros')(decoded)
+    if layers == 2: 
+        encoded = Dense(150, activation = 'relu')(input_img)
+        encoded = Dense(120, activation = 'relu')(encoded)
 
-#Autoencoder: 
-autoencoder = Model(input_img, decoded)
+        decoded = Dense(150, activation = 'relu')(encoded)
+        decoded = Dense(784, activation='sigmoid')(decoded)
 
-#Encoder:
-encoder = Model(input=input_img, output=encoded)
+    if layers == 1:
+        encoded = Dense(150, activation = 'relu')(input_img)
 
-#Decoder
-encoded_input_1 = Input(shape=(90,))
-encoded_input_2 = Input(shape=(120,))
-encoded_input_3 = Input(shape=(150,))
+        decoded = Dense(784, activation='sigmoid')(encoded)
 
-decoder_layer_1 = autoencoder.layers[-3]
-decoder_layer_2 = autoencoder.layers[-2]
-decoder_layer_3 = autoencoder.layers[-1]
+    #-----------Autoencoder: 
+    if layers != 0:
+        autoencoder = Model(input_img, decoded)
+        sgd = optimizers.SGD(lr=2, momentum=0.9, decay=0, nesterov=False)
+        autoencoder.compile(optimizer=sgd, loss='mean_squared_error', metrics=['mae'])
+        autoencoder.fit(train, train,
+                        epochs=my_epochs,
+                        batch_size= 256, #default
+                        shuffle=True, verbose = 0,
+                        validation_data=(test, test))
 
-decoder_1 = Model(input = encoded_input_1, output = decoder_layer_1(encoded_input_1))
-decoder_2 = Model(input = encoded_input_2, output = decoder_layer_2(encoded_input_2))
-decoder_3 = Model(input = encoded_input_3, output = decoder_layer_3(encoded_input_3))
+        #-----------Encoder:
+        encoder = Model(input=input_img, output=encoded)
+    
+    #-----------Decoder
+    if layers == 3: 
+        encoded_input_1 = Input(shape=(90,))
+        encoded_input_2 = Input(shape=(120,))
+        encoded_input_3 = Input(shape=(150,))
 
-#Training in pre-training phase
-# sgd = optimizers.SGD(lr=5, momentum=0, decay=0, nesterov=False)
-ada = optimizers.Adadelta(lr=1, epsilon=None, decay=0.0)
-autoencoder.compile(optimizer=ada, loss='binary_crossentropy')
+        decoder_layer_1 = autoencoder.layers[-3]
+        decoder_layer_2 = autoencoder.layers[-2]
+        decoder_layer_3 = autoencoder.layers[-1]
 
-train, train_targets = data_handling.read_train_dataset()
-test, test_targets = data_handling.read_test_dataset()
-train_targets = np_utils.to_categorical(train_targets)
-test_targets = np_utils.to_categorical(test_targets)
+        decoder_1 = Model(input = encoded_input_1, output = decoder_layer_1(encoded_input_1))
+        decoder_2 = Model(input = encoded_input_2, output = decoder_layer_2(encoded_input_2))
+        decoder_3 = Model(input = encoded_input_3, output = decoder_layer_3(encoded_input_3))
 
-autoencoder.fit(train, train,
-                epochs=200,
-                batch_size=10, #default
-                shuffle=True,
-                validation_data=(test, test))
+        encoded_imgs = encoder.predict(train)
+        decoded_imgs = decoder_1.predict(encoded_imgs) 
+        decoded_imgs = decoder_2.predict(decoded_imgs)
+        decoded_imgs = decoder_3.predict(decoded_imgs)
 
-#-------------pretraining done, adding classifier-------------------
+    if layers == 2: 
+        encoded_input_2 = Input(shape=(120,))
+        encoded_input_3 = Input(shape=(150,))
 
-#adding one more "layer"
-decoded = Dense(10, activation='sigmoid', kernel_initializer='random_normal', bias_initializer='zeros')(decoded)
+        decoder_layer_2 = autoencoder.layers[-2]
+        decoder_layer_3 = autoencoder.layers[-1]
 
-classifier = Model(input = input_img, output = decoded)
+        decoder_2 = Model(input = encoded_input_2, output = decoder_layer_2(encoded_input_2))
+        decoder_3 = Model(input = encoded_input_3, output = decoder_layer_3(encoded_input_3))
 
-#training classifier
-# sgd = optimizers.SGD(lr=5, momentum=0, decay=0, nesterov=False)
-ada = optimizers.Adadelta(lr=1, epsilon=None, decay=0.0)
-classifier.compile(optimizer=ada, loss='binary_crossentropy')
+        encoded_imgs = encoder.predict(train)
+        decoded_imgs = decoder_2.predict(encoded_imgs)
+        decoded_imgs = decoder_3.predict(decoded_imgs)
 
-hist = classifier.fit(train, train_targets, 
-                epochs = 200, 
-                batch_size = 10, 
-                shuffle = True,
-                validation_data=(test, test_targets))
+    if layers == 1: 
+        encoded_input_3 = Input(shape=(150,))
+        decoder_layer_3 = autoencoder.layers[-1]
+        decoder_3 = Model(input = encoded_input_3, output = decoder_layer_3(encoded_input_3))
 
-predictions = classifier.predict(test)
+        encoded_imgs = encoder.predict(train)
+        decoded_imgs = decoder_3.predict(encoded_imgs)
 
-encoded_imgs = encoder.predict(test)
-decoded_imgs = decoder_1.predict(encoded_imgs)
-decoded_imgs = decoder_2.predict(decoded_imgs)
-decoded_imgs = decoder_3.predict(decoded_imgs)
+#--------- evaluate using Logistic Regression
+    logistic = LogisticRegression(C=10.0,random_state=0,max_iter=my_epochs)
+    
+    if layers == 0: 
+        logistic.fit(train, train_targets)
+    else:
+        logistic.fit(decoded_imgs, train_targets)
 
+        # dim = train_data.shape[1]
+    print('\nCLASSIFICATION ERRORS')
+    print(classification_report(test_targets, logistic.predict(test)))
 
-evaluate(test_targets, predictions) #classifier or decdoded_imgs?
-#--------------------------------------------------------------------------------
-#Plotting digits: 
-n = 10  
-plt.figure(figsize=(20, 4))
-for i in range(n):
-    ax = plt.subplot(2, n, i + 1)
-    plt.imshow(test[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+    if layers == 3:
+        encoded_imgs = encoder.predict(test)
+        decoded_imgs = decoder_1.predict(encoded_imgs) 
+        decoded_imgs = decoder_2.predict(decoded_imgs)
+        decoded_imgs = decoder_3.predict(decoded_imgs)
 
-    ax = plt.subplot(2, n, i + 1 + n)
-    plt.imshow(decoded_imgs[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+    if layers == 2: 
+        encoded_imgs = encoder.predict(test)
+        decoded_imgs = decoder_2.predict(encoded_imgs)
+        decoded_imgs = decoder_3.predict(decoded_imgs)
 
-    # np.save('test_3_layers_75epochs_0.3lr.npy', hist.history)
-plt.show()
+    if layers == 1: 
+        encoded_imgs = encoder.predict(test)
+        decoded_imgs = decoder_3.predict(encoded_imgs)
 
-# Plotting images for layers: 
+#---------Plotting digits: ---------------------
+    example_indexs = [18, 3, 7, 0, 2, 1, 14, 8, 6, 5]
+    n = 10  
+    plt.figure(figsize=(20, 4))
+    for i in range(n):
+        ax = plt.subplot(2, n, i + 1)
+        plt.imshow(test[example_indexs[i]].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
 
-weights_3 = decoder_3.get_weights()[0]
-weights_2 = decoder_2.get_weights()[0]
-weights_1 = decoder_1.get_weights()[0]
+        ax = plt.subplot(2, n, i + 1 + n)
+        plt.imshow(decoded_imgs[example_indexs[i]].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+            
+        # np.save('test_3_layers_75epochs_0.3lr.npy', hist.history)
+    plt.show()
 
-# print(np.array(weights_3).shape)
-# print(np.array(weights_2).shape)
-# print(np.array(weights_1).shape)
+#---------Plotting weights: ---------------
+    if layers == 3: 
+        weights_3 = decoder_3.get_weights()[0]
+        weights_2 = decoder_2.get_weights()[0]
+        weights_1 = decoder_1.get_weights()[0]
 
-plt.figure(figsize=(10, 10))
-for i in range(150):
-    plt.subplot(10, 15, i + 1)
-    plt.imshow(weights_3[i].reshape((28, 28)), cmap=plt.cm.gray_r,
-                interpolation='nearest')
-    plt.xticks(())
-    plt.yticks(())
-plt.suptitle('150 components extracted by autoencoder layer 1', fontsize=16)
-plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
-plt.show()
+        plt.figure(figsize=(10, 10))
+        for i in range(150):
+            plt.subplot(10, 15, i + 1)
+            plt.imshow(weights_3[i].reshape((28, 28)), cmap=plt.cm.gray_r,
+                        interpolation='nearest')
+            plt.xticks(())
+            plt.yticks(())
+        plt.suptitle('150 components extracted by autoencoder layer 1', fontsize=16)
+        plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
+        plt.show()
 
-plt.figure(figsize=(10, 10))
-for i in range(120):
-    plt.subplot(10, 12, i + 1)
-    plt.imshow(weights_2[i].reshape((10, 15)), cmap=plt.cm.gray_r,
-                interpolation='nearest')
-    plt.xticks(())
-    plt.yticks(())
-plt.suptitle('120 components extracted by autoencoder layer 2', fontsize=16)
-plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
-plt.show()
+        plt.figure(figsize=(10, 10))
+        for i in range(120):
+            plt.subplot(10, 12, i + 1)
+            plt.imshow(weights_2[i].reshape((10, 15)), cmap=plt.cm.gray_r,
+                        interpolation='nearest')
+            plt.xticks(())
+            plt.yticks(())
+        plt.suptitle('120 components extracted by autoencoder layer 2', fontsize=16)
+        plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
+        plt.show()
 
-plt.figure(figsize=(10, 10))
-for i in range(90):
-    plt.subplot(10, 10, i + 1)
-    plt.imshow(weights_1[i].reshape((10, 12)), cmap=plt.cm.gray_r,
-                interpolation='nearest')
-    plt.xticks(())
-    plt.yticks(())
-plt.suptitle('90 components extracted by autoencoder layer 3', fontsize=16)
-plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
-plt.show()
+        plt.figure(figsize=(10, 10))
+        for i in range(90):
+            plt.subplot(10, 10, i + 1)
+            plt.imshow(weights_1[i].reshape((10, 12)), cmap=plt.cm.gray_r,
+                        interpolation='nearest')
+            plt.xticks(())
+            plt.yticks(())
+        plt.suptitle('90 components extracted by autoencoder layer 3', fontsize=16)
+        plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
+        plt.show()
+
+if __name__ == "__main__":
+    main()
